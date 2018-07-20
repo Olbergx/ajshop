67559571dfc24a48d6ad6944365c77f76fe456cd
olbergx <olbergx@mail.ru>
2018-07-20 09:53:39 +0400

    коментарии

--- cart.php
+++ cart.php
@@ -5,6 +5,7 @@
 <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="js/jquery.maskedinput.min.js"></script>
 <script src="js/script.js"></script>
+<!-- API ключ от Google -->
 <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
